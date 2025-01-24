c06cbd0ec9e5d89cf778b31f6bf74c47c16a16db
Sreysros <sreysrosdara12@gmail.com>
2025-01-24 13:40:26 +0700

    Start Layout

---
+++ resources/views/master.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Roboto+Slab:wght@500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="{{url('assets/theme.css')}}">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+      <title>Document</title>
+</head>
+<body>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+            <a class="navbar-brand logo " href="#"><img src="https://i.pinimg.com/736x/c5/7e/49/c57e4937c15a57d5226263659a533ab6.jpg" alt="" wight="80px" height="80px"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
+                  aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarColor01">
+                  <ul class="navbar-nav mb-2 mb-lg-0">
+                  <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                  </li>
+                  <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                  </li>
+                  </ul>
+            </div>
+      </div>
+      </nav>
+</body>
+</html>
